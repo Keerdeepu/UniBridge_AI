622dcb6493c829764faa9d9e054de770f784152c
M. Keerthi Reddy <136425048+Keerdeepu@users.noreply.github.com>
2025-09-20 18:21:19 +0530

    Add files via upload

---
+++ Team2_Internship_Finder/Frontend1/App.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import InternForm from "./components/InternForm";
+
+function App() {
+  return (
+    <div>
+      <InternForm/>
+    </div>
+  );
+}
+
+export default App;
