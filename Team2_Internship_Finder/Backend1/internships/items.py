b3f14e654411ad03b2890dc8a812ab8955244d97
M. Keerthi Reddy <136425048+Keerdeepu@users.noreply.github.com>
2025-11-07 08:51:37 +0530

    Delete Team2_Internship_Finder directory

--- Team2_Internship_Finder/Backend1/internships/items.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-class InternshipsItem(scrapy.Item):
-    title = scrapy.Field()
-    company = scrapy.Field()
-    location = scrapy.Field()
-    link = scrapy.Field()
-    source = scrapy.Field()
-
